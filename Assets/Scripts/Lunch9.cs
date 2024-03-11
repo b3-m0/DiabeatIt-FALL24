92bc348fc3eec574d2c5058cfd46603ffa6a987c
de-hwang <daniel.eghwang@gmail.com>
2024-03-11 03:00:33 -0700

    Added Nutrition Minigame

---
+++ Assets/Scripts/Lunch9.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lunch9 : MonoBehaviour
+{
+  public void sceneOneTransition() {
+    SceneManager.LoadScene("Lunch9");
+  }
+}
