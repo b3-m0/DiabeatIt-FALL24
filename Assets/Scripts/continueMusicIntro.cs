391f112e7102275a9f86f9d3d02efce053834a0c
clydexmarty09 <cmarty@ucsd.edu>
2024-05-14 10:32:47 -0700

    added some comments to continueMusicIntro

--- Assets/Scripts/continueMusicIntro.cs
+++ Assets/Scripts/continueMusicIntro.cs
@@ -1,8 +1,17 @@
+/* This file is used to continue introduction background music 
+ * It ensures the background music continues playing across different scenes
+ * in the game EXCEPT if the player returns the to the Main Menu.
+ * The background music plays when the game begins and while scenes load.
+ 
+ * It contains the continueMusicIntro class and preceeding methods 
+ */
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+ /* Below are the imports */
+using System.Collections;   // enables utilization of collection structures : not relevant in this particular file 
+using System.Collections.Generic;  // enabnles access of the Generic class like List<T>
+using UnityEngine;  // this is needed for Unity Scripting 
+using UnityEngine.SceneManagement;  // needed for scene management like loading and unloading scenes 
+
 
 public class continueMusicIntro : MonoBehaviour
 {
