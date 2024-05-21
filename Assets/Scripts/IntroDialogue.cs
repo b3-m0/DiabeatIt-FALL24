7f243264aa2a68460df8cebf8bebbd66e9bb7cdc
clydexmarty09 <cmarty@ucsd.edu>
2024-05-21 14:33:44 -0700

    added IntroDialogue documentation

--- Assets/Scripts/IntroDialogue.cs
+++ Assets/Scripts/IntroDialogue.cs
@@ -1,11 +1,19 @@
+/*
+  This file is used to load IntroDialogue
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/* The line below declares a class that inherits the MonoBehaviour */ 
 public class IntroDialogue : MonoBehaviour
 {
+
+  /* The method below is reposonsible for initiating sceneIntroDialogue transitions */ 
   public void sceneIntroDialogueTransition() {
+    
+    /* uses the SceneManager namespace to Load IntroDialogue */
     SceneManager.LoadScene("IntroDialogue");
   }
 }
