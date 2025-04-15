21f38e1c9eb8340f47ab57572d18222881a8879e
b3-m0 <bemiyamoto24@gmail.com>
2025-04-15 16:34:05 -0700

    delete old code folder, rename spr24 file

--- Assets/Scripts/sceneLoadSpr24.cs
+++ Assets/Scripts/sceneLoadSpr24.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class main_menu : MonoBehaviour
+public class sceneLoadSpr24 : MonoBehaviour
 {
     public void PlayGame()
     {
